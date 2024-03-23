6989258d9b1a680855caa4d95fdad1131e4e5383
BajanVlogs <kjolly544@gmail.com>
2024-03-23 14:53:39 -0400

    Update Main.php

--- src/georgianYT/Hammer/Main.php
+++ src/georgianYT/Hammer/Main.php
@@ -47,33 +47,22 @@ class Main extends PluginBase implements Listener {
 
     public function onBlockBreak(BlockBreakEvent $event){
         $player = $event->getPlayer();
-        $item = $event->getItem();
+        $item = $player->getInventory()->getItemInHand(); // Get the item the player is holding
         $block = $event->getBlock();
 
         if($item instanceof DiamondPickaxe && $item->getCustomName() === TextFormat::RED . "Hammer"){
             $level = $player->getLevel();
-            for($count = 0; $count >= -2; $count--){
-                $bpos = $level->getBlockIdAt($block->x + 1, $block->y + $count, $block->z);
-                if($bpos !== Item::BEDROCK && $bpos !== Item::OBSIDIAN){
-                    $level->setBlockIdAt($block->x + 1, $block->y + $count, $block->z, Item::AIR);
-                    $item = Item::get($bpos, 0, 1);  
-                    $level->dropItem(new Vector3($block->x + 1, $block->y + $count, $block->z), $item);
+            $radius = 1; // 3x3 hole, so radius is 1 block in each direction
+            for($x = -$radius; $x <= $radius; $x++) {
+                for($z = -$radius; $z <= $radius; $z++) {
+                    $pos = $block->add($x, 0, $z);
+                    $bpos = $level->getBlockIdAt($pos->x, $pos->y, $pos->z);
+                    if($bpos !== Item::BEDROCK && $bpos !== Item::OBSIDIAN) {
+                        $level->setBlockIdAt($pos->x, $pos->y, $pos->z, Item::AIR);
+                        $item = Item::get($bpos, 0, 1);  
+                        $level->dropItem($pos, $item);
+                    }
                 }
-                // Repeat similar checks for other adjacent blocks
-                // ...
-            }
-            // Additional actions for breaking blocks below
-            $bpos = $level->getBlockIdAt($block->x, $block->y - 1, $block->z);
-            if($bpos !== Item::BEDROCK && $bpos !== Item::OBSIDIAN){
-                $level->setBlockIdAt($block->x, $block->y - 1, $block->z, Item::AIR);
-                $item = Item::get($bpos, 0, 1);  
-                $level->dropItem(new Vector3($block->x, $block->y - 1, $block->z), $item);
-            }
-            $bpos = $level->getBlockIdAt($block->x, $block->y - 2, $block->z);
-            if($bpos !== Item::BEDROCK && $bpos !== Item::OBSIDIAN){
-                $level->setBlockIdAt($block->x, $block->y - 2, $block->z, Item::AIR);
-                $item = Item::get($bpos, 0, 1);  
-                $level->dropItem(new Vector3($block->x, $block->y - 2, $block->z), $item);
             }
         }
     }
